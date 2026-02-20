03c0b977da80dc93a4d288ed164059d6972b6242
vanvinhacc1-gif <vanvinhacc1@gmail.com>
2026-02-20 22:36:23 +0700

    Update main.mm

--- main.mm
+++ main.mm
@@ -8,38 +8,41 @@
 void showLogin() {
     dispatch_async(dispatch_get_main_queue(), ^{
         UIWindow *window = [UIApplication sharedApplication].keyWindow;
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:KH_NAME message:@"BẢN QUYỀN VAN VINH" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"XÁC THỰC CUỐI CÙNG" message:@"Nhập Key để kiểm tra hạn dùng" preferredStyle:UIAlertControllerStyleAlert];
+        [alert addTextFieldWithConfigurationHandler:^(UITextField *t){ t.placeholder=@"Key..."; }];
         
-        [alert addTextFieldWithConfigurationHandler:^(UITextField *txt) {
-            txt.placeholder = @"Nhập Key...";
-            txt.secureTextEntry = YES;
-        }];
-        
-        [alert addAction:[UIAlertAction actionWithTitle:@"Đăng Nhập" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-            NSString *userKey = alert.textFields.firstObject.text;
-            // Thêm mã ngẫu nhiên vào URL để ép Server phải check mới, không dùng lại Session cũ
-            int random = arc4random_uniform(10000);
-            NSString *urlStr = [NSString stringWithFormat:@"https://keyauth.win/api/1.2/?type=login&name=%@&ownerid=%@&secret=%@&key=%@&ver=1.0&r=%d", KH_NAME, KH_OWNERID, KH_SECRET, userKey, random];
+        [alert addAction:[UIAlertAction actionWithTitle:@"Kích Hoạt" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            NSString *key = alert.textFields.firstObject.text;
+            NSString *url = [NSString stringWithFormat:@"https://keyauth.win/api/1.2/?type=login&name=%@&ownerid=%@&secret=%@&key=%@", KH_NAME, KH_OWNERID, KH_SECRET, key];
             
-            [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:urlStr] completionHandler:^(NSData *data, NSURLResponse *res, NSError *err) {
+            [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:url] completionHandler:^(NSData *data, NSURLResponse *res, NSError *err) {
                 if (data) {
                     NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-                    BOOL success = [[json objectForKey:@"success"] boolValue];
-                    NSString *msg = [[json objectForKey:@"message"] lowercaseString]; // Chuyển tin nhắn về chữ thường để check cho chuẩn
-                    
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        // KIỂM TRA CỰC GẮT: SUCCESS PHẢI LÀ TRUE VÀ TRONG MESSAGE KHÔNG ĐƯỢC CÓ CHỮ "EXPIRED" (HẾT HẠN)
-                        if (success && ![msg containsString:@"expired"] && ![msg containsString:@"invalid"]) {
-                            UIAlertController *ok = [UIAlertController alertControllerWithTitle:@"Thành Công" message:@"Chào mừng VanVinh!" preferredStyle:UIAlertControllerStyleAlert];
-                            [ok addAction:[UIAlertAction actionWithTitle:@"Vào Game" style:UIAlertActionStyleDefault handler:nil]];
-                            [window.rootViewController presentViewController:ok animated:YES completion:nil];
-                        } else {
-                            // NẾU CÓ CHỮ "EXPIRED" TRONG MESSAGE, NÓ SẼ BÁO LỖI NGAY DÙ SERVER CÓ TRẢ VỀ SUCCESS = TRUE
-                            NSString *finalMsg = (success && [msg containsString:@"expired"]) ? @"Key của mày đã hết hạn rồi!" : @"Key không đúng hoặc đã hết hạn!";
-                            UIAlertController *no = [UIAlertController alertControllerWithTitle:@"TỪ CHỐI" message:finalMsg preferredStyle:UIAlertControllerStyleAlert];
-                            [no addAction:[UIAlertAction actionWithTitle:@"Nhập lại" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *a){ showLogin(); }]];
-                            [window.rootViewController presentViewController:no animated:YES completion:nil];
+                    // LẤY TRỰC TIẾP SỐ NGÀY CÒN LẠI TỪ SERVER
+                    id info = [json objectForKey:@"info"];
+                    if (info && [info isKindOfClass:[NSDictionary class]]) {
+                        NSArray *subscriptions = [info objectForKey:@"subscriptions"];
+                        if (subscriptions.count > 0) {
+                            long long expiry = [[subscriptions[0] objectForKey:@"expiry"] longLongValue];
+                            long long now = (long long)[[NSDate date] timeIntervalSince1970];
+                            
+                            dispatch_async(dispatch_get_main_queue(), ^{
+                                // TỰ SO SÁNH: NẾU THỜI GIAN HẾT HẠN NHỎ HƠN THỜI GIAN HIỆN TẠI -> CÚT
+                                if (expiry > now) {
+                                    UIAlertController *ok = [UIAlertController alertControllerWithTitle:@"Hợp Lệ" message:@"Chào VanVinh, mời vào game!" preferredStyle:UIAlertControllerStyleAlert];
+                                    [ok addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+                                    [window.rootViewController presentViewController:ok animated:YES completion:nil];
+                                } else {
+                                    UIAlertController *no = [UIAlertController alertControllerWithTitle:@"HẾT HẠN" message:@"Key này đã hết thời gian sử dụng!" preferredStyle:UIAlertControllerStyleAlert];
+                                    [no addAction:[UIAlertAction actionWithTitle:@"Thoát" style:UIAlertActionStyleDestructive handler:^(id a){ showLogin(); }]];
+                                    [window.rootViewController presentViewController:no animated:YES completion:nil];
+                                }
+                            });
+                            return;
                         }
+                    }
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        showLogin(); // Sai key hoặc lỗi info thì bắt nhập lại
                     });
                 }
             }] resume];
@@ -48,9 +51,6 @@ void showLogin() {
     });
 }
 
-__attribute__((constructor))
-static void init() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        showLogin();
-    });
+__attribute__((constructor)) static void init() {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{ showLogin(); });
 }
