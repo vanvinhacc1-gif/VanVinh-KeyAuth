d8294db191a45d41ec200320ca699a5bb7ad1bbc
vanvinhacc1-gif <vanvinhacc1@gmail.com>
2026-02-20 22:25:28 +0700

    Update main.mm

--- main.mm
+++ main.mm
@@ -8,33 +8,43 @@
 void showLogin() {
     dispatch_async(dispatch_get_main_queue(), ^{
         UIWindow *window = [UIApplication sharedApplication].keyWindow;
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:KH_NAME message:@"Xác Thực Bản Quyền - VanVinh" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:KH_NAME 
+                                                                       message:@"XÁC THỰC BẢN QUYỀN - VAN VINH" 
+                                                                preferredStyle:UIAlertControllerStyleAlert];
         
         [alert addTextFieldWithConfigurationHandler:^(UITextField *txt) {
-            txt.placeholder = @"Nhập Key của mày vào đây...";
+            txt.placeholder = @"Nhập Key của mày...";
             txt.secureTextEntry = YES;
         }];
         
         [alert addAction:[UIAlertAction actionWithTitle:@"Đăng Nhập" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
             NSString *userKey = alert.textFields.firstObject.text;
-            NSString *urlStr = [NSString stringWithFormat:@"https://keyauth.win/api/1.2/?type=login&name=%@&ownerid=%@&secret=%@&key=%@", KH_NAME, KH_OWNERID, KH_SECRET, userKey];
+            // Ép Server check mới bằng cách thêm timestamp vào URL
+            NSString *urlStr = [NSString stringWithFormat:@"https://keyauth.win/api/1.2/?type=login&name=%@&ownerid=%@&secret=%@&key=%@&sessionid=", KH_NAME, KH_OWNERID, KH_SECRET, userKey];
             
             [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:urlStr] completionHandler:^(NSData *data, NSURLResponse *res, NSError *err) {
                 if (data) {
                     NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
                     BOOL success = [[json objectForKey:@"success"] boolValue];
-                    NSString *msg = [json objectForKey:@"message"];
-                    
+                    NSString *message = [json objectForKey:@"message"];
+
                     dispatch_async(dispatch_get_main_queue(), ^{
-                        if (success) {
-                            // CHỈ KHI KEY ĐÚNG VÀ CÒN HẠN MỚI CHẠY ĐOẠN NÀY
-                            UIAlertController *ok = [UIAlertController alertControllerWithTitle:@"Thành Công" message:@"Chào mừng VanVinh đã quay trở lại!" preferredStyle:UIAlertControllerStyleAlert];
+                        // LOGIC KHÓA CỨNG: CHỈ KHI SUCCESS LÀ TRUE VÀ KHÔNG CÓ LỖI HẾT HẠN
+                        if (success && ![message containsString:@"expired"]) {
+                            UIAlertController *ok = [UIAlertController alertControllerWithTitle:@"Thành Công" 
+                                                                                       message:@"Chào mừng VanVinh đã quay trở lại!" 
+                                                                                preferredStyle:UIAlertControllerStyleAlert];
                             [ok addAction:[UIAlertAction actionWithTitle:@"Vào Game" style:UIAlertActionStyleDefault handler:nil]];
                             [window.rootViewController presentViewController:ok animated:YES completion:nil];
                         } else {
-                            // KEY SAI HOẶC HẾT HẠN SẼ HIỆN LỖI VÀ BẮT NHẬP LẠI
-                            UIAlertController *no = [UIAlertController alertControllerWithTitle:@"Lỗi Truy Cập" message:msg preferredStyle:UIAlertControllerStyleAlert];
-                            [no addAction:[UIAlertAction actionWithTitle:@"Nhập lại" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *a){ showLogin(); }]];
+                            // NẾU SAI HOẶC HẾT HẠN, HIỆN LỖI VÀ QUAY LẠI BẢNG ĐĂNG NHẬP
+                            NSString *errorMsg = message ? message : @"Key không hợp lệ hoặc đã hết hạn!";
+                            UIAlertController *no = [UIAlertController alertControllerWithTitle:@"TRUY CẬP BỊ TỪ CHỐI" 
+                                                                                       message:errorMsg 
+                                                                                preferredStyle:UIAlertControllerStyleAlert];
+                            [no addAction:[UIAlertAction actionWithTitle:@"Thử lại" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *a){
+                                showLogin(); 
+                            }]];
                             [window.rootViewController presentViewController:no animated:YES completion:nil];
                         }
                     });
@@ -47,7 +57,7 @@ void showLogin() {
 
 __attribute__((constructor))
 static void init() {
-    // Đợi 5 giây cho game load xong UI rồi mới hiện bảng login
+    // Đợi 5 giây cho game ổn định rồi mới hiện bảng login
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         showLogin();
     });
