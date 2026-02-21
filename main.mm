fb525e071a2d2107003d1c1c48c294b5d2c9389a
vanvinhacc1-gif <vanvinhacc1@gmail.com>
2026-02-21 11:25:51 +0700

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#include <dlfcn.h> // Thư viện để gọi file dylib khác
+#include <dlfcn.h>
 
 #define KH_NAME    @"VanVinhiOS" 
 #define KH_OWNERID @"gnIVuUid3U"
@@ -9,11 +9,19 @@
 void showLogin() {
     dispatch_async(dispatch_get_main_queue(), ^{
         UIWindow *window = [UIApplication sharedApplication].keyWindow;
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"VAN VINH - VER 2.0" message:@"NHẬP KEY ĐỂ MỞ MENU HACK" preferredStyle:UIAlertControllerStyleAlert];
-        [alert addTextFieldWithConfigurationHandler:^(UITextField *t){ t.placeholder=@"Key..."; }];
+        // Tạo bảng nhập Key
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"XÁC THỰC VAN VINH" 
+                                                                       message:@"Vui lòng nhập Key để tiếp tục" 
+                                                                preferredStyle:UIAlertControllerStyleAlert];
         
-        [alert addAction:[UIAlertAction actionWithTitle:@"Đăng Nhập" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [alert addTextFieldWithConfigurationHandler:^(UITextField *t){ 
+            t.placeholder=@"Nhập Key tại đây..."; 
+            t.secureTextEntry = YES;
+        }];
+        
+        [alert addAction:[UIAlertAction actionWithTitle:@"Kích Hoạt" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
             NSString *key = alert.textFields.firstObject.text;
+            // Ép Version 2.0 để chặn mọi Session cũ
             NSString *url = [NSString stringWithFormat:@"https://keyauth.win/api/1.2/?type=login&name=%@&ownerid=%@&secret=%@&key=%@&ver=2.0", KH_NAME, KH_OWNERID, KH_SECRET, key];
             
             [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:url] completionHandler:^(NSData *data, NSURLResponse *res, NSError *err) {
@@ -23,27 +31,38 @@ void showLogin() {
                     NSString *msg = [[json objectForKey:@"message"] lowercaseString];
                     
                     dispatch_async(dispatch_get_main_queue(), ^{
+                        // CHỈ CHO VÀO KHI SUCCESS = TRUE VÀ MESSAGE KHÔNG CHỨA CHỮ "EXPIRED"
                         if (success && ![msg containsString:@"expired"]) {
+                            // LOAD MENU MOD TỪ THƯ MỤC APP
+                            dlopen([[NSString stringWithFormat:@"%@/aimkill Lol.dylib", [[NSBundle mainBundle] bundlePath]] UTF8String], RTLD_NOW);
                             
-                            // === ĐÂY LÀ CHỖ QUAN TRỌNG: GỌI MENU MOD ===
-                            // Nó sẽ load file có tên là MenuMod.dylib nằm trong thư mục App
-                            dlopen([[NSString stringWithFormat:@"%@/MenuMod.dylib", [[NSBundle mainBundle] bundlePath]] UTF8String], RTLD_NOW);
-                            
-                            UIAlertController *ok = [UIAlertController alertControllerWithTitle:@"Thành Công" message:@"Menu Mod đã được kích hoạt!" preferredStyle:UIAlertControllerStyleAlert];
-                            [ok addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+                            UIAlertController *ok = [UIAlertController alertControllerWithTitle:@"THÀNH CÔNG" message:@"Đã kích hoạt Menu Hack!" preferredStyle:UIAlertControllerStyleAlert];
+                            [ok addAction:[UIAlertAction actionWithTitle:@"Vào Game" style:UIAlertActionStyleDefault handler:nil]];
                             [window.rootViewController presentViewController:ok animated:YES completion:nil];
                         } else {
-                            // Hiện bảng từ chối nếu key sai hoặc hết hạn
-                            showLogin(); 
+                            // SAI LÀ KHÓA CỨNG - HIỆN BẢNG LỖI
+                            UIAlertController *no = [UIAlertController alertControllerWithTitle:@"TRUY CẬP BỊ TỪ CHỐI" 
+                                                                                       message:@"Key sai, hết hạn hoặc không tồn tại!" 
+                                                                                preferredStyle:UIAlertControllerStyleAlert];
+                            // Nút "Thử lại" này sẽ gọi lại login, không cho nó tắt bảng đi để vào game
+                            [no addAction:[UIAlertAction actionWithTitle:@"Nhập lại Key" style:UIAlertActionStyleDestructive handler:^(id a){ 
+                                showLogin(); 
+                            }]];
+                            [window.rootViewController presentViewController:no animated:YES completion:nil];
                         }
                     });
                 }
             }] resume];
         }]];
+        
+        // Ngăn chặn việc bấm ra ngoài để tắt bảng (tùy phiên bản iOS)
         [window.rootViewController presentViewController:alert animated:YES completion:nil];
     });
 }
 
 __attribute__((constructor)) static void init() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{ showLogin(); });
+    // Đợi 5 giây để Game load xong mới hiện Login
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{ 
+        showLogin(); 
+    });
 }
